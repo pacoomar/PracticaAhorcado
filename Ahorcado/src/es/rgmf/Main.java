2ca2f12a9adaa71103bbde25993a0ea339e38cf6
Román Martínez <rgmf@riseup.net>
2022-03-09 12:15:40 +0100

    Primer commit.

---
+++ Ahorcado/src/es/rgmf/Main.java
@@ -0,0 +1,47 @@
+package es.rgmf;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String frase;
+        String[] frases;
+        char[] fraseOculta;
+        Scanner entrada = new Scanner(System.in);
+        Path path;
+
+        // Carga el fichero con las frases
+        path = Paths.get("src/es/rgmf/frases.txt");
+        try {
+            frases = Files.lines(path).toArray(String[]::new);
+        } catch (IOException e) {
+            e.printStackTrace();
+            frases = new String[] {"Hola Amigo"};
+        }
+
+        // Prepara la frase
+        frase = frases[(int) (Math.random() * frases.length)];
+        fraseOculta = new char[frase.length()];
+
+        // Construye la frase oculta.
+        for (int i = 0; i < frase.length(); i++) {
+            if (frase.charAt(i) == ' ') {
+                fraseOculta[i] = ' ';
+            } else {
+                fraseOculta[i] = '_';
+            }
+        }
+
+        // Muestra la frase oculta.
+        System.out.println(fraseOculta);
+
+        // TODO Completa el programa.
+
+        entrada.close();
+    }
+}
